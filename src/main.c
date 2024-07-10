c2e13fa02f7776fb72efa8ae84fa072f1c80dbd0
bigbearishappy <953308023@qq.com>
2024-07-10 08:03:27 +0000

    add demo code for lcd backlight enable
    Signed-off-by: bigbearishappy <953308023@qq.com>

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 
 static const struct gpio_dt_spec lra_en =
    GPIO_DT_SPEC_GET_OR(DT_NODELABEL(lra_en_pin), gpios, {0});
+static const struct gpio_dt_spec lcd_bk_en =
+   GPIO_DT_SPEC_GET_OR(DT_NODELABEL(lcd_bk_en_pin), gpios, {0});
 
 int main(void)
 {
@@ -13,12 +15,16 @@ int main(void)
 	if(lra_en.port)
 		gpio_pin_configure_dt(&lra_en, GPIO_OUTPUT);
 
+	if(lcd_bk_en.port)
+		gpio_pin_configure_dt(&lcd_bk_en, GPIO_OUTPUT);
+
 	while(1) {
 		printk("hello cnt:%d\n", cnt++);
 		k_msleep(1000);
-		if(lra_en.port) {
+		if(lra_en.port) 
 			cnt % 2 ? gpio_pin_set_dt(&lra_en, 1) : gpio_pin_set_dt(&lra_en, 0);
-		}
+		if(lcd_bk_en.port) 
+			cnt % 2 ? gpio_pin_set_dt(&lcd_bk_en, 1) : gpio_pin_set_dt(&lcd_bk_en, 0);
 	}
 	return 0;
 }
