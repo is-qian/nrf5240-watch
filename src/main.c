595e76cb7a7addab822ee2eb4db85dc630779a76
bigbearishappy <953308023@qq.com>
2024-07-12 09:05:16 +0000

    add demo for uart work with p0.02 and P0.03
    Signed-off-by: bigbearishappy <953308023@qq.com>

--- src/main.c
+++ src/main.c
@@ -4,10 +4,19 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/flash.h>
 #include <zephyr/device.h>
+#include <zephyr/drivers/uart.h>
 
 //micros
 #define SPI_FLASH_TEST_REGION_OFFSET 0xff000
 #define SPI_FLASH_SECTOR_SIZE        4096
+
+#define UART_DEVICE_NODE DT_CHOSEN(zephyr_shell_uart)
+//#define UART_DEVICE_NODE DT_CHOSEN(seeed_debug_uart)
+#define MSG_SIZE 32
+K_MSGQ_DEFINE(uart_msgq, MSG_SIZE, 10, 4);
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+static char rx_buf[MSG_SIZE];
+static int rx_buf_pos;
 
 //gpio output
 static const struct gpio_dt_spec lra_en =
@@ -135,23 +144,91 @@ void test_qspi_flash(const struct device *flash_dev)
         }
 }
 
+static void serial_cb(const struct device *dev, void *user_data)
+{
+        uint8_t c;
+
+        if (!uart_irq_update(uart_dev)) {
+                return;
+        }
+
+        if (!uart_irq_rx_ready(uart_dev)) {
+                return;
+        }
+
+        /* read until FIFO empty */
+        while (uart_fifo_read(uart_dev, &c, 1) == 1) {
+                if ((c == '\n' || c == '\r') && rx_buf_pos > 0) {
+                        /* terminate string */
+                        rx_buf[rx_buf_pos] = '\0';
+
+                        /* if queue is full, message is silently dropped */
+                        k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
+
+                        /* reset the buffer (it was copied to the msgq) */
+                        rx_buf_pos = 0;
+                } else if (rx_buf_pos < (sizeof(rx_buf) - 1)) {
+                        rx_buf[rx_buf_pos++] = c;
+                }
+                /* else: characters beyond buffer size are dropped */
+        }
+}
+
+static void print_uart(char *buf)
+{
+        int msg_len = strlen(buf);
+
+        for (int i = 0; i < msg_len; i++) {
+                uart_poll_out(uart_dev, buf[i]);
+        }
+}
+
+void test_uart(void)
+{
+        if (!device_is_ready(uart_dev)) {
+                printk("UART device not found!");
+        }
+	int ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+        if (ret < 0) {
+                if (ret == -ENOTSUP) {
+                        printk("Interrupt-driven UART API support not enabled\n");
+                } else if (ret == -ENOSYS) {
+                        printk("UART device does not support interrupt-driven API\n");
+                } else {
+                        printk("Error setting UART callback: %d\n", ret);
+                }
+        }
+        uart_irq_rx_enable(uart_dev);
+
+        print_uart("Hello! I'm your echo bot.\r\n");
+        print_uart("Tell me something and press enter:\r\n");
+}
+
 int main(void)
 {
 	int cnt = 0;
 
         const struct device *flash_dev = DEVICE_DT_GET(DT_ALIAS(spi_flash0));
 
+        char tx_buf[MSG_SIZE];
+
         if (!device_is_ready(flash_dev)) {
                 printk("%s: device not ready.\n", flash_dev->name);
         }
 
 	test_qspi_flash(flash_dev);
+	test_uart();
 
 	while(1) {
 		printk("hello cnt:%d\n", cnt++);
 		k_msleep(1000);
 		test_output();
 		test_input();
+		if(k_msgq_get(&uart_msgq, &tx_buf, K_NO_WAIT) == 0) {
+	        	print_uart("Echo: ");
+	        	print_uart(tx_buf);
+	        	print_uart("\r\n");
+		}
 	}
 	return 0;
 }
