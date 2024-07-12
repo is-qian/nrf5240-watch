012e06440e600cad72934e5d6adc2879bae9f5d6
bigbearishappy <953308023@qq.com>
2024-07-12 01:52:37 +0000

    add qspi flash test code
    Signed-off-by: bigbearishappy <953308023@qq.com>

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include <zephyr/sys/printk.h>
 
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/device.h>
+
+//micros
+#define SPI_FLASH_TEST_REGION_OFFSET 0xff000
+#define SPI_FLASH_SECTOR_SIZE        4096
 
 //gpio output
 static const struct gpio_dt_spec lra_en =
@@ -69,11 +75,77 @@ void test_input(void)
 	printk("button1:%d button2:%d button3:%d button4:%d\n", button1, button2, button3, button4);
 }
 
+void test_qspi_flash(const struct device *flash_dev)
+{
+        const uint8_t expected[] = { 0x55, 0xaa, 0x66, 0x99 };
+        const size_t len = sizeof(expected);
+        uint8_t buf[sizeof(expected)];
+        int rc;
+
+        printf("\nPerform test on single sector");
+        /* Write protection needs to be disabled before each write or
+         * erase, since the flash component turns on write protection
+         * automatically after completion of write and erase
+         * operations.
+         */
+        printf("\nTest 1: Flash erase\n");
+
+        /* Full flash erase if SPI_FLASH_TEST_REGION_OFFSET = 0 and
+         * SPI_FLASH_SECTOR_SIZE = flash size
+         */
+        rc = flash_erase(flash_dev, SPI_FLASH_TEST_REGION_OFFSET,
+                         SPI_FLASH_SECTOR_SIZE);
+        if (rc != 0) {
+                printf("Flash erase failed! %d\n", rc);
+        } else {
+                printf("Flash erase succeeded!\n");
+        }
+
+        printf("\nTest 2: Flash write\n");
+
+        printf("Attempting to write %zu bytes\n", len);
+        rc = flash_write(flash_dev, SPI_FLASH_TEST_REGION_OFFSET, expected, len);
+        if (rc != 0) {
+                printf("Flash write failed! %d\n", rc);
+                return;
+        }
+
+        memset(buf, 0, len);
+        rc = flash_read(flash_dev, SPI_FLASH_TEST_REGION_OFFSET, buf, len);
+        if (rc != 0) {
+                printf("Flash read failed! %d\n", rc);
+                return;
+        }
+
+        if (memcmp(expected, buf, len) == 0) {
+                printf("Data read matches data written. Good!!\n");
+        } else {
+                const uint8_t *wp = expected;
+                const uint8_t *rp = buf;
+                const uint8_t *rpe = rp + len;
+
+                printf("Data read does not match data written!!\n");
+                while (rp < rpe) {
+                        printf("%08x wrote %02x read %02x %s\n",
+                               (uint32_t)(SPI_FLASH_TEST_REGION_OFFSET + (rp - buf)),
+                               *wp, *rp, (*rp == *wp) ? "match" : "MISMATCH");
+                        ++rp;
+                        ++wp;
+                }
+        }
+}
+
 int main(void)
 {
 	int cnt = 0;
 
+        const struct device *flash_dev = DEVICE_DT_GET(DT_ALIAS(spi_flash0));
 
+        if (!device_is_ready(flash_dev)) {
+                printk("%s: device not ready.\n", flash_dev->name);
+        }
+
+	test_qspi_flash(flash_dev);
 
 	while(1) {
 		printk("hello cnt:%d\n", cnt++);
