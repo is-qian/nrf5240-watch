850b4c8ce162ef26f12604a6ec5f16e8c5e78553
bigbearishappy <953308023@qq.com>
2025-01-03 02:23:25 +0000

    optimize the usage of INA226 python script example: `python3 ft4232hq_get_power_consume.py 0x40`
    Signed-off-by: bigbearishappy <953308023@qq.com>

--- tools/ft4232hq_get_power_consume.py
+++ tools/ft4232hq_get_power_consume.py
@@ -1,13 +1,44 @@
 from pyftdi.i2c import I2cController, I2cNackError
+from pyftdi.ftdi import Ftdi
+from pyftdi.misc import add_custom_devices
+import re
+import io
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: ft4232hq_get_power_consume.py <id_hex>")
+    sys.exit(1)
+
+slave_id = int(sys.argv[1], 16)
+
+# Get dev name of ft4232
+add_custom_devices(Ftdi, force_hex=True)
+
+output = io.StringIO()
+sys.stdout = output
+
+Ftdi.show_devices()
+
+sys.stdout = sys.__stdout__
+
+devlist = output.getvalue()
+
+match = re.search(r'ftdi://ftdi:4232:1:\d+/1', devlist) 
+
+if match:
+    dev_str = match.group(0)
+    print(dev_str)
+else:
+    print("No device found")
 
 # Instantiate an I2C controller
 i2c = I2cController()
 
 # Configure the first interface (IF/1) of the FTDI device as an I2C master
-i2c.configure('ftdi://ftdi:4232:1:1d/1')
+i2c.configure(dev_str)
 
 # Get a port to an I2C slave device
-slave = i2c.get_port(0x40)
+slave = i2c.get_port(slave_id)
 
 # reset the chip
 slave.exchange([0x00, 0xc1, 0x27], 1)
@@ -17,16 +48,16 @@ slave.exchange([0x05, 0x0c, 0x80], 1)
 
 # Read a register from the I2C slave
 x = slave.read_from(0x01, 2)
-#print("reg shuntvol(0x01):"+x.hex())
 print("shunt voltage:", round(int.from_bytes(x, byteorder='big')*0.0025, 4),"mV")
+
 x = slave.read_from(0x02, 2)
-#print("reg busvol (0x02):"+x.hex())
 print("bus voltage:",int.from_bytes(x, byteorder='big')*1.25,"mV")
+
 x = slave.read_from(0x03, 2)
-#print("reg power(0x03):"+x.hex())
 print("power:", int.from_bytes(x, byteorder='big')*80/1000, "mW")
+
 x = slave.read_from(0x04, 2)
-#print("reg curr(0x04):"+x.hex())
-print("current:", int.from_bytes(x, byteorder='big')*3.2/1000, "mA")
+print("current:", round(int.from_bytes(x, byteorder='big')*3.2/1000, 4), "mA")
+
 x = slave.read_from(0x05, 2)
 print("reg cali(0x05):"+x.hex())
